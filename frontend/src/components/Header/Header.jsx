41a3b449514bb4dd338ec84fad0ab23c0fd1ef07
Amogh S V <amoghsv19@gmail.com>
2023-04-17 10:56:48 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,23 +6,30 @@ import 'reactjs-popup/dist/index.css';
 import Basket from './Basket.jsx';
 import './popupExtra.css'
 import Upload from './Upload.jsx';
-
-import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Header(props) {
-  
+
   const { onAdd, onRemove, setItems, reRender, user, updateUser } = props;
+  
+  const history = useHistory();
 
   var s = {
     height: "2cm",
     width: "5cm"
   }
 
+  const redirectme = () =>{
+    history.push("/orderHistory");
+    window.location.reload();
+
+  }
+
   return (
     <header className="text-gray-400 bg-gray-900 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-          {/* <img style={s} className="imgLogo" src='http://localhost:9002/getIMG' /> */}
+          <img style={s} className="imgLogo" src='http://localhost:9002/getIMG' />
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
 
@@ -35,12 +42,15 @@ export default function Header(props) {
             user && user._id ? <a className="mr-5 hover:text-white">Help</a> : <a></a>
           }
         </nav>
-
+        {user && user._id ?
+          <button className="mybutton" onClick={redirectme}> Order History &nbsp;</button> : <></>
+        }
         <Router>
           <div className="flex justify-between">
-            { user && user._id ?
+            {user && user._id ?
+
               <Popup
-                trigger={<button className="mybutton"> Cart </button>}
+                trigger={<button className="mybutton"> &nbsp; Cart &nbsp; </button>}
                 modal
                 nested
               >
@@ -71,13 +81,13 @@ export default function Header(props) {
                     </div>
                   </div>
                 )}
-              </Popup>:
+              </Popup> :
               <div></div>
             }
             {
               user && user._id ?
                 <Popup
-                  trigger={<button className="mybutton"> Upload </button>}
+                  trigger={<button className="mybutton">&nbsp; Upload &nbsp; </button>}
                   modal
                   nested
                 >
@@ -111,6 +121,7 @@ export default function Header(props) {
                 <></>
             }
           </div>
+          &nbsp;
           <Sidebar
             user={user}
             updateUser={updateUser} />
