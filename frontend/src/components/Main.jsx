5c7182458c92a7c8ec1242e0193894b4560d2db6
Amogh S V <amoghsv19@gmail.com>
2023-04-18 14:23:41 +0530

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -26,7 +26,7 @@ export default function Main(props) {
 
   const getItems = async () => {
     try {
-      console.log("test commit amogh 01")
+      console.log("test commit amogh 001")
       const { data } = await axios.get("http://localhost:5002/items")
       return data
     } catch (error) {
